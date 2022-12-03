9c190b96beb8a6061a534b6129a0ea09793f9fdf
Patrick Shushereba <pshushereba@gmail.com>
2022-12-02 20:02:25 -0800

    Added src directory and other dependencies.

---
+++ src/plugin.js
@@ -0,0 +1,7 @@
+/**
+ * Function to register the plugin in the application.
+ * @param {Vue} app - The Vue instance.
+ * @param {SocketIO} socket - The SocketIO instance.
+ * @param {Object} options - Plugin configuration options.
+ */
+function install(app, socket, options) {}

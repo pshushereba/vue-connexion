dac15ffdf4af09fcfab72258169d103fd55f7957
Patrick Shushereba <pshushereba@gmail.com>
2022-12-02 18:47:59 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vue Connexion
+
+A package providing Socket.io bindings for Vue 3 with Composition API.
---
+++ main.js
@@ -0,0 +1,16 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;
